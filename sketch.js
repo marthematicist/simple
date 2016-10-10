200157655aaa681809423d3199edd1b4731a8395
marthematicist <hite.32@gmail.com>
2016-10-10 08:57:14 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,10 @@
 function setup() {
-
+  createCanvas( windowWidth , windowHeight );
+  background( 255 , 255 , 255 );
 }
 
 function draw() {
-
+  noStroke();
+  fill( 20 , 147 , 232 , 50);
+  ellipse( mouseX , mouseY , 100 , 100 );
 }
